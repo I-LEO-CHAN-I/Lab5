d4b382a92c7c093124d15a27bb5a4065c4bed306
Semyon Popov <samuro2311@gmail.com>
2024-11-06 19:23:47 +0700

    Код для MainActivity

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,20 +1,85 @@
 package com.example.lab5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Currency
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var saleSlider : SeekBar
+    private lateinit var saleText : TextView
+    private lateinit var costValue : EditText
+    private lateinit var usdCheck : RadioButton
+    private lateinit var eurCheck : RadioButton
+    private lateinit var pndCheck : RadioButton
+    private lateinit var okButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+
+        saleSlider = findViewById(R.id.saleValSlider)
+        saleText = findViewById(R.id.saleValueText)
+        usdCheck = findViewById(R.id.usdRadioButton)
+        eurCheck = findViewById(R.id.euroRadioButton)
+        pndCheck = findViewById(R.id.poundRadioButton)
+        okButton = findViewById(R.id.calcButton)
+
+        var saleVal : Double
+
+        saleSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                saleText.text = progress.toString() + "%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
+
+        okButton.setOnClickListener() {
+            var currency : Int = 1
+            var currencyName : String = "Default"
+            var baseCost = costValue.text.toString().toDouble()
+
+            if (usdCheck.isChecked) {
+                currency = 93
+                currencyName = "долларах"
+            }
+            if (eurCheck.isChecked) {
+                currency = 105
+                currencyName = "евро"
+            }
+            if (pndCheck.isChecked) {
+                currency = 126
+                currencyName = "фунтах"
+            }
+
+            val finalCost = baseCost * currency * (1 - (saleSlider.progress/100))
+
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("FINAL_COAST", finalCost)
+                putExtra("CURRENCY_NAME", currencyName)
+            }
+            startActivity (intent)
         }
     }
 }
